21280e70887042306d2a1c59487e94cfb7f6bcf7
Dan Lynch <pyramation@gmail.com>
2024-02-19 22:04:12 +0100

    more pkgs

--- src/packages.ts
+++ src/packages.ts
@@ -2,16 +2,63 @@ import { Packages } from "./types";
 
 // Structured as categories with their respective packages
 export const packages: Packages = {
-    "cosmos-kit": ['@cosmos-kit/core', '@cosmos-kit/react'],
-    "cosmwasm": ['@cosmwasm/ts-codegen'],
-    "interchain UI": ['@interchain-ui/react'],
-    "telescope": ['@cosmology/telescope', '@osmonauts/telescope'],
-    "osmosis": ['@osmonauts/math', 'osmojs'],
-    "cosmology": [
-        '@chain-registry/client', '@chain-registry/types', '@chain-registry/utils',
-        '@cosmology/cli', 'chain-registry', 'cosmology', 'cosmos-kit', 'create-cosmos-app'
+    "cosmos-kit": [
+        'cosmos-kit',
+        '@cosmos-kit/core',
+        '@cosmos-kit/react'
     ],
-    "launchql": ['@launchql/cli', 'libpg-query', 'pgsql-parser'],
+    "cosmwasm": ['@cosmwasm/ts-codegen'],
+    "interchain-ui": [
+        '@interchain-ui/react'
+    ],
+    "telescope": [
+        '@cosmology/telescope',
+        '@cosmology/lcd',
+        '@cosmology/ast',
+        '@osmonauts/telescope',
+        '@osmonauts/lcd',
+        '@osmonauts/ast'
+    ],
+    "dydx": ['@dydxprotocol/v4-client-js'],
+    "stargaze": [
+        'stargazejs',
+        '@stargaze-zone/chain',
+        '@stargaze-zone/contracts',
+        'stargaze-query'
+    ],
+    "stride": ['stridejs'],
+    "quicksilver": ['quicksilverjs'],
+    "juno": [
+        'juno-network',
+        '@juno-network/assets'
+    ],
+    "osmosis": ['@osmonauts/math', 'osmojs'],
+    "chain-registry": [
+        '@chain-registry/client',
+        '@chain-registry/types',
+        '@chain-registry/keplr',
+        '@chain-registry/cosmostation',
+        '@chain-registry/osmosis',
+        '@chain-registry/juno',
+        '@chain-registry/assets',
+        '@chain-registry/utils',
+        'chain-registry'
+    ],
+    "cosmology": [
+        '@cosmology/cli',
+        'cosmology',
+        'create-cosmos-app'
+    ],
+    "launchql": [
+        'graphile-query',
+        'graphile-settings',
+        'graphile-testing',
+        'pg-ast',
+        '@launchql/cli',
+        '@launchql/server',
+        'libpg-query',
+        'pgsql-parser'
+    ],
     "protobufs": [
         '@protobufs/cosmos', '@protobufs/google', '@protobufs/gogoproto', '@protobufs/cosmwasm',
         '@protobufs/tendermint', '@protobufs/ibc', '@protobufs/cosmos_proto', '@protobufs/osmosis',
